b8935a2faeeb39cf458a9d60c1db7e91ef903a14
IngLuisNavas <ingluisnavas@gmail.com>
2022-02-24 21:52:57 -0500

    actualizacion

--- script.js
+++ script.js
@@ -2,9 +2,3 @@ var pantalla = document.querySelector("canvas");
 var pincel = pantalla.getContext("2d");
 pincel.fillStyle = "lightgrey";
 pincel.fillRect(0, 0, 1200, 800);
-
-
-function limpiarPantalla() {
-
-    pincel.clearRect(0, 0, 1200, 800);
-}
